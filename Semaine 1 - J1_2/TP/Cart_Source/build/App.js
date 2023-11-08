6f8dfd3dd5179b13e577ca7f32691445b2565513
Anabelle <anabelle.piret@hotmail.com>
2023-11-08 09:02:47 +0100

    add tp

---
+++ Semaine 1 - J1_2/TP/Cart_Source/build/App.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Importez les bons fichiers et définition et une fois que vous avez récupérez tous les products mappez ce dernier
+// pour extraire uniquement les produits dont l'option delivery est "special"
+const Product_1 = require("./Product");
+const MockProducts_1 = require("./data/MockProducts");
+const products = MockProducts_1.MockDetails.map((detail) => {
+    let deliver = MockProducts_1.MockDelivery.find((delivery) => delivery.id == detail.id);
+    return new Product_1.Product(detail, deliver ? deliver.delivery : MockProducts_1.Delivery.DoorToDoor);
+});
+const specialProduct = products.filter((product) => {
+    return product.option === MockProducts_1.Delivery.Special;
+});
+console.log(specialProduct);
--- Semaine 1 - J1_2/TP/Cart_Source/src/App.ts
+++ Semaine 1 - J1_2/TP/Cart_Source/src/App.ts
@@ -1,2 +1,16 @@
 // Importez les bons fichiers et définition et une fois que vous avez récupérez tous les products mappez ce dernier
 // pour extraire uniquement les produits dont l'option delivery est "special"
+import { Product } from "./Product"
+import { Details, Delivery, MockDetails, MockDelivery } from './data/MockProducts';
+
+
+const products: Array<Product<Details, Delivery>> = MockDetails.map((detail) => {
+    let deliver = MockDelivery.find((delivery) => delivery.id == detail.id);
+    return new Product(detail, deliver ? deliver.delivery : Delivery.DoorToDoor);
+});
+
+const specialProduct: Array<Product<Details, Delivery>> = products.filter((product) => {
+    return product.option === Delivery.Special;
+});
+
+console.log(specialProduct);
