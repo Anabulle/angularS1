ad98018e2b712a90a3a4f101f980494cd1e1d554
Anabelle <anabelle.piret@hotmail.com>
2023-12-06 12:04:42 +0100

    add new project

---
+++ app-school/ecole-app/src/app/etudiants/etudiants.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-etudiants',
+  templateUrl: './etudiants.component.html',
+  styleUrls: ['./etudiants.component.sass']
+})
+export class EtudiantsComponent {
+
+}
