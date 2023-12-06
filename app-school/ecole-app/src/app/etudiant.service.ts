3a6b8d02c90d200b5b5fa09b68ea1111af9da7e0
Anabelle <anabelle.piret@hotmail.com>
2023-12-06 17:40:26 +0100

    add crud

--- app-school/ecole-app/src/app/etudiant.service.ts
+++ app-school/ecole-app/src/app/etudiant.service.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Etudiant } from './etudiant.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EtudiantService {
+  private apiUrl = '/api/etudiants';
 
   constructor(private http: HttpClient) { }
   
-  getEtudiants() {
-    return this.http.get('http://localhost:3100/etudiants');
+  getEtudiants(): Observable<Etudiant[]> {
+    return this.http.get<Etudiant[]>(this.apiUrl);
   }
+
+  deleteEtudiant(etudiantId: string): Observable<any> {
+    const url = `${this.apiUrl}/${etudiantId}`;
+    return this.http.delete<any>(url);
+  }
+
+
 }
