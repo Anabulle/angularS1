b642364e4b9724b85ed468b5ff5d4c1a1825a89a
Anabelle <anabelle.piret@hotmail.com>
2023-12-07 09:57:46 +0100

    update ajouter etudiants

--- app-school/ecole-app/src/app/ajouter-etudiant/ajouter-etudiant.component.ts
+++ app-school/ecole-app/src/app/ajouter-etudiant/ajouter-etudiant.component.ts
@@ -8,15 +8,19 @@ import { EtudiantService } from '../etudiant.service';
   styleUrls: ['./ajouter-etudiant.component.css']
 })
 export class AjouterEtudiantComponent {
-  nouvelEtudiant: Etudiant = new Etudiant('', '', '');
+  nouvelEtudiant: Etudiant = {
+    nom: '',
+    prenom: '',
+    classe: ''
+  };
 
   constructor(private etudiantService: EtudiantService) { }
 
   ajouterEtudiant() {
     this.etudiantService.addEtudiant(this.nouvelEtudiant).subscribe({
       next: (response: Etudiant) => {
-        console.log( response);
-        this.nouvelEtudiant = new Etudiant('', '', '')
+        console.log('Étudiant ajouté avec succès', response);
+        this.nouvelEtudiant = { nom: '', prenom: '', classe: '' }; 
       },
       error: (error: any) => {
         console.error('Erreur lors de l\'ajout de l\'étudiant', error);
