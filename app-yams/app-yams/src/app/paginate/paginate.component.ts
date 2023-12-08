22b134b7da304308f797caf9857044b0e6f0b181
Anabelle <anabelle.piret@hotmail.com>
2023-12-08 16:57:10 +0100

    update paginate

--- app-yams/app-yams/src/app/paginate/paginate.component.ts
+++ app-yams/app-yams/src/app/paginate/paginate.component.ts
@@ -12,9 +12,11 @@ export class PaginateComponent implements OnInit {
 
   currentPage = 1;
   paginatedData: any[] = [];
+  totalPages: number[] = [];
 
   ngOnInit(): void {
     this.paginate();
+    this.generatePageNumbers();
   }
 
   paginate(): void {
@@ -42,4 +44,25 @@ export class PaginateComponent implements OnInit {
   getTotalPages(): number {
     return Math.ceil(this.data.length / this.perPage);
   }
+
+  generatePageNumbers(): void {
+    const total = this.getTotalPages();
+    this.totalPages = Array.from({ length: total }, (_, index) => index + 1);
+    if (this.totalPages.length === 0) {
+      this.totalPages.push(1); // Ajouter la page 1 si aucune page n'existe
+    }
+  
+    // Vérifier si la page actuelle est supérieure au nombre total de pages
+    if (this.currentPage > total) {
+      this.goToPage(total); // Définir la page actuelle comme dernière page si elle est supérieure
+    }
+  }
+
+  goToPage(pageNumber: number): void {
+    if (pageNumber >= 1 && pageNumber <= this.getTotalPages()) {
+      this.currentPage = pageNumber;
+      this.paginate();
+    }
+  }
+  
 }
