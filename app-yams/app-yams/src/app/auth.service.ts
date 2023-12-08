0c75cb2fae7b235ec0c4dca6a0db30b8ebbf182b
Anabelle <anabelle.piret@hotmail.com>
2023-12-08 10:18:33 +0100

    update login

--- app-yams/app-yams/src/app/auth.service.ts
+++ app-yams/app-yams/src/app/auth.service.ts
@@ -1,26 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private isAuthenticated: boolean = false;
+  public authChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
 
   login(username: string, password: string): boolean {
-
     if (username === 'Toto' && password === 'Tutu') {
       this.isAuthenticated = true;
+      this.authChanged.emit(true); // Émet un événement pour indiquer un changement d'authentification
     } else {
       this.isAuthenticated = false;
+      this.authChanged.emit(false); // Émet un événement pour indiquer un changement d'authentification
     }
     return this.isAuthenticated;
   }
 
   logout(): void {
     this.isAuthenticated = false;
+    this.authChanged.emit(false); // Émet un événement pour indiquer un changement d'authentification
   }
 
   isLoggedIn(): boolean {
     return this.isAuthenticated;
   }
 }
+
